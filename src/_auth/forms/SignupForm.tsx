e0d63c310c2a680e259c440f02c406bcec8f2d1f
Aivaras Jastrumskis <aivarasj12@gmail.com>
2024-02-13 18:06:23 +0200

    update

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -17,12 +17,14 @@ import { useForm } from "react-hook-form";
 import { SignupValidation } from "@/lib/validation";
 import { z } from "zod";
 import { Loader } from "lucide-react";
-import { createUserAccount } from "@/lib/appwrite/api";
-import { sign } from "crypto";
+import { useCreateUserAccount, useSignInAccount } from "@/lib/react-query/queriesAndMutations";
 
 const SignupForm = () => {
   const { toast } = useToast()
-  const isLoading = false;
+
+  const { mutateAsync: createUserAccount, isLoading: isCreatingAccount } = useCreateUserAccount();
+
+  const { mutateAsync: signInAccount, isLoading: isSigningIn } = useSignInAccount();
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof SignupValidation>>({
@@ -35,6 +37,8 @@ const SignupForm = () => {
     },
   });
 
+
+
   // 2. Define a submit handler.
   async function onSubmit(values: z.infer<typeof SignupValidation>) {
     const newUser = await createUserAccount(values);
@@ -43,7 +47,14 @@ const SignupForm = () => {
       return toast({ title: "Sign Up failed. Please try again."})
     }
 
-    // const session = await signInAccount()
+    const session = await signInAccount({
+      email: values.email,
+      password: values.password,
+    })
+
+    if (!session) {
+      return toast({ title: "Sign Up failed. Please try again."})
+    }
   }
 
   return (
@@ -118,7 +129,7 @@ const SignupForm = () => {
               )}
             />
             <Button type="submit" className="shad-button_primary">
-              {isLoading ? (
+              {isCreatingUser ? (
                 <div className="flex-center gap-2">
                   <Loader /> Loading...
                 </div>
